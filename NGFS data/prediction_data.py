0e23a1794cd3d7384b11140f6fd7d7af4670c18d
Theoseo <ktsim@tudelft.nl>
2026-05-28 13:51:21 +0200

    Implemented HMM for temperature

--- NGFS data/prediction_data.py
+++ NGFS data/prediction_data.py
@@ -1,6 +1,43 @@
 # Pulling working data from NGFS data
 import pandas as pd
 
+
+# scenario_list = [
+#     "Net Zero 2050",
+#     "Delayed transition",
+#     "Current Policies",
+#     "Fragmented World",
+# ]
+#
+# # NiGEM data
+# nigem_regions = [
+#     "NiGEM NGFS v1.24.2|United States",
+#     "NiGEM NGFS v1.24.2|Japan",
+#     "NiGEM NGFS v1.24.2|United Kingdom",
+#     "NiGEM NGFS v1.24.2|World",
+#     "NiGEM NGFS v1.24.2|Europe",
+#     "NiGEM NGFS v1.24.2|China",
+# ]
+# nigem_var_list = ["Equity", "Interest Rate", "Inflation", "GDP", "Exchange"]
+# nigem_vars_join = "|".join(nigem_var_list)
+#
+# df = pd.read_excel("NiGEM_data.xlsx")
+# working_nigem = df[
+#     (
+#         df["Model"].str.contains("REMIND-MAgPIE 3.3-4.8", case=False)
+#         & df["Region"].isin(nigem_regions)
+#         & df["Variable"].str.contains(nigem_vars_join, case=False)
+#         & df["Scenario"].isin(scenario_list)
+#     )
+# ]
+# working_nigem.to_csv("NiGEM_working_set.csv")
+
+
+# IAM data
+model = "REMIND-MAgPIE 3.3-4.8"
+iam_regions = ["United", "Japan", "EU 28", "China", "World"]
+iam_regions_join = "|".join(iam_regions)
+iam_var_list = ["Emissions|C02", "Price|Carbon"]
 scenario_list = [
     "Net Zero 2050",
     "Delayed transition",
@@ -8,46 +45,20 @@ scenario_list = [
     "Fragmented World",
 ]
 
-# NiGEM data
-nigem_regions = [
-    "NiGEM NGFS v1.24.2|United States",
-    "NiGEM NGFS v1.24.2|Japan",
-    "NiGEM NGFS v1.24.2|United Kingdom",
-    "NiGEM NGFS v1.24.2|World",
-    "NiGEM NGFS v1.24.2|Europe",
-    "NiGEM NGFS v1.24.2|China",
-]
-nigem_var_list = ["Equity", "Interest Rate", "Inflation", "GDP", "Exchange"]
-nigem_vars_join = "|".join(nigem_var_list)
+df = pd.read_excel("IAM_data.xlsx")
+working_iam = df[
+    (df["Scenario"].isin(scenario_list) 
+            &
+            df["Model"].str.contains(model)
+            &
+            df["Region"].str.contains(iam_regions_join, case=False)
+            &
+            (df["Variable"].isin(iam_var_list)
+             |
+             df["Variable"].str.contains("^Primary Energy", regex=True)
+             |
+             df["Variable"].str.contains("Temperature", case=False))
 
-df = pd.read_excel("NiGEM_data.xlsx")
-working_nigem = df[
-    (
-        df["Model"].str.contains("REMIND-MAgPIE 3.3-4.8", case=False)
-        & df["Region"].isin(nigem_regions)
-        & df["Variable"].str.contains(nigem_vars_join, case=False)
-        & df["Scenario"].isin(scenario_list)
     )
 ]
-working_nigem.to_csv("NiGEM_working_set.csv")
-
-# IAM data
-# model = "REMIND-MAgPIE 3.3-4.8"
-# iam_regions = ["United", "Japan", "EU 28", "China", "World"]
-# iam_regions_join = "|".join(iam_regions)
-# iam_var_list = ["Emissions|C02", "Price|Carbon", "Temperature"]
-#
-# df = pd.read_excel("IAM_data.xlsx")
-# working_iam = df[
-#     (
-#             df["Model"].str.contains(model)
-#             &
-#             df["Region"].str.contains(iam_regions_join, case=False)
-#             &
-#             (df["Variable"].isin(iam_var_list)
-#              |
-#              df["Variable"].str.contains("^Primary Energy", regex=True))
-#
-#     )
-# ]
-# working_iam.to_csv("IAM_working_set.csv")
+working_iam.to_csv("IAM_working_set.csv")
--- hmm_training.py
+++ hmm_training.py
@@ -2,38 +2,103 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import hmmlearn.hmm as hmm
-from sklearn.model_selection import StratifiedKFold
+import numpy as np
+from hmmlearn.hmm import GaussianHMM
+from sklearn.utils import check_random_state
 
 # https://wmo.int/resources/dashboards/global-mean-temperature-1850-2024
 # Compared to mean in 1850-1900
 
 # %%
-#TODO Implement cross-validation to optimize amount of components
-
-# %%
 df = pd.read_csv(r"Historical data/mean_temp.csv")
 df.set_index("year", inplace=True)
-df = df.mean(axis=1)
+# Take the mean of the models
+df['mean'] = df.mean(axis=1)
 
-
+# Modify bins to account for values outside of min/max
+# Bin the observations in intervals of (a, b])
+min_temp, max_temp = df['mean'].min(), df['mean'].max()
+bin_amount = 10
+in_len = (max_temp - min_temp)/bin_amount
+# Define bins
+bins = np.linspace(min_temp, max_temp, bin_amount + 1)
+bins[0], bins[-1] = -np.inf, np.inf
+# Change first and last bins to include -inf, inf respectively
+# Use index to refer back to bins so it is compatible with the HMM
+df['bins'] = pd.cut(df['mean'], bins=bins, labels=False, right=True)
 
 # TODO implement Hidden markov
 # %%
 # Show the histogram
 fig, ax = plt.subplots(figsize=(12, 6))
-ax.hist(df, bins=10)
+ax.hist(df['mean'], bins=10)
 ax.set_xlabel("Surface temperature (C)")
 ax.set_ylabel("Count")
 plt.show()
-
 # %%
-lst = df.to_numpy()
-print(type(lst))
-# %%
-# Reshaping the data
-data = df.to_numpy()
+data = df["bins"].to_numpy()
 data = data.reshape(len(df), 1)
-# Training the HMM model
-remodel = hmm.GaussianHMM(n_components=4, covariance_type="full", n_iter=1000)
-remodel.fit(data)
-test = remodel.predict(data)
+
+# TODO Optimize amount of states using AIC, BIC and log-likelihood
+# Optimizing model for amount of components
+rs = check_random_state(546)
+max_comp = 10
+for n in range(2, max_comp + 1):
+    best_ll, best_model = -np.inf, hmm.GaussianHMM()
+    for i in range(10):
+        h = hmm.GaussianHMM(n_components=n, covariance_type="full", n_iter=100, random_state=rs)
+        out = h.fit(data)
+        score = h.score(data)
+        if best_ll < score:
+            best_ll = score
+            print(score)
+            best_model = h
+    print(f"Completed n = {n}")
+# %%
+# Plot state counts of data
+pred = best_model.predict(data)
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.hist(pred, bins=best_model.n_components)
+ax.set_xlabel("State")
+ax.set_ylabel("Count")
+plt.show()
+# %%
+# Wrangle data to extract necessary data
+iam_data = pd.read_csv(r"NGFS data/IAM_working_set.csv")
+temp = iam_data[iam_data["Variable"].str.contains("AR6 climate diagnostics|Surface Temperature ("
+                                                  "GSAT)|MAGICCv7.5.3|50.0th Percentile", case=False, regex=False)]
+temp = temp.drop(["Model", "Region", "Variable", "Unit", "Unnamed: 0"], axis=1)
+temp = temp.groupby(["Scenario"], as_index=True).mean()
+# %%
+# Predict temperature from NGFS and calculate likelihood
+scenario_list = [
+    "Net Zero 2050",
+    "Delayed transition",
+    "Current Policies",
+    "Fragmented World",
+]
+scores = {}
+for i, row in temp.iterrows():
+    sc = str(row.name)
+    scenario_data = pd.cut(row, bins=bins, labels=False)
+    data = scenario_data.to_numpy()
+    data = data.reshape(len(scenario_data), 1)
+    states = best_model.predict(data)
+    sc_score = best_model.score(data)
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    plt.title(sc)
+    ax.hist(states, bins=best_model.n_components)
+    ax.set_xlabel("State")
+    ax.set_ylabel("Count")
+    plt.show()
+
+    scores[sc] = best_model.score(data)
+
+# log-likelihood of -11,008
+norm_fac = sum(scores.values())
+for k, v in scores.items():
+    scores[k] = v/norm_fac
+
+
+
